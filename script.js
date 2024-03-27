18818772fcc0b3d2f07273a0359419418da7e6ec
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-03-27 13:25:59 +0100

    Add images and project descriptions

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 window.onload = function() {
   var projectImages = document.getElementsByClassName("project-image");
   var projectDivs = document.getElementsByClassName("card");
-  var coloredPhotos = ["images/project1.png"]
-  var blackWhitePhotos = ["images/project1bw.png"];
-  
-  console.log(projectDivs.length);
-  console.log(coloredPhotos[0]);
+  var coloredPhotos = ["images/project1.png", "images/project2.png", "images/project3.png", "images/project4.png"]
+  var blackWhitePhotos = ["images/project1bw.png", "images/project2bw.png", "images/project3bw.png", "images/project4bw.png"];
   
   for (var i = 0; i < projectDivs.length; i++) {
     projectDivs[i].index = i;
