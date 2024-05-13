ed5749a36d3da6b10b89bd93eeed602b5a87aee3
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-05-13 06:05:43 +0200

    Create React project with Vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-teal-300 border-2 border-dashed border-yellow-400 text-red-700'>Hello, portfolio!</h1>        
+    </>
+  )
+}
+
+export default App
