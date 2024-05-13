7e8574070c5019e348247f809b9a35fc32c091ba
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-05-13 06:24:57 +0200

    Add components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import About from './components/About.tsx'
+import Navbar from './components/Navbar.tsx'
+import Landing from './components/Landing.tsx'
+import Projects from './components/Projects.tsx'
+import Blog from './components/Blog.tsx'
+import Form from './components/Form.tsx'
+import Footer from './components/Footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <Landing />
+    <About />
+    <Projects />
+    <Blog />
+    <Form />
+    <Footer />
   </React.StrictMode>,
 )
