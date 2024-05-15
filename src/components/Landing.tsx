64ead4dda69f6672f43978daaaceaf281b859686
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-05-15 17:54:15 +0200

    Add menu icons

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -4,7 +4,7 @@ export default function Landing() {
             <p className="font-medium text-lg text-zinc-800">Hi, my name is</p>
             <h1 className="text-3xl font-black text-zinc-800 pb-5">Amadeja Obšteter Privošnik</h1>
             <p className="font-medium text-zinc-800 pb-5">I like coding visually appealing and user-friendly websites.</p>
-            <a href="#about" className="font-bold text-zinc-800 w-fit">Find out more about me</a>
+            <a href="#about" className=" link font-bold text-zinc-800 w-fit">Find out more about me</a>
         </section>
     )
 }
