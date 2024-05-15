8de5f0ef9a7b17af82ec3bffc697cc1956062797
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-05-15 14:18:33 +0200

    Add font

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
                     <li className="px-3 hover:bg-violet-400 active:bg-green-300"><a href="#blog">Blog</a></li>
                     <li className="px-3 hover:bg-violet-400 active:bg-green-300"><a href="#contact">Contact</a></li>
                 </ul>
-                <button className="self-start text-3xl font-light font-sans lg:hidden pt-1" onClick={toggleNavbar}>Menu</button>
+                <button className="self-start text-3xl font-light lg:hidden pt-1" onClick={toggleNavbar}>Menu</button>
             </nav>
             <ul className={`flex flex-col items-center justify-center gap-10 text-2xl font-light p-4 h-screen w-screen ${isOpen ? 'translate-y-0' : '-translate-y-full'} ease-in-out duration-500 bg-pink-100 absolute z-50`}>
                     <button className="absolute top-5 right-11 text-3xl" onClick={toggleNavbar}>Close</button>
