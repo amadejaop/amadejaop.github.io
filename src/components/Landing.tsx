bfd9b130440ebd0ce8e8f3c3554d213b81cf7172
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-05-15 16:43:18 +0200

    Change logo

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,7 +1,7 @@
 export default function Landing() {
     return (
         <section className="flex flex-col justify-end pb-28 px-7 h-128 w-full">
-            <p className="font-medium text-lg text-zinc-800">Hi, My name is</p>
+            <p className="font-medium text-lg text-zinc-800">Hi, my name is</p>
             <h1 className="text-3xl font-black text-zinc-800 pb-5">Amadeja Obšteter Privošnik</h1>
             <p className="font-medium text-zinc-800 pb-5">I like coding visually appealing and user-friendly websites.</p>
             <a href="#about" className="font-bold text-zinc-800 w-fit">Find out more about me</a>
