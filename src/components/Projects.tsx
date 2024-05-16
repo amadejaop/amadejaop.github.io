4c17a0b4e4ada01dd584b91fd9033d785e2d1bf9
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-05-16 14:24:29 +0200

    Update projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,10 +2,10 @@ export default function Projects() {
     return (
         <>
             <section id="projects" className="flex flex-col justify-center items-center bg-white py-20 px-7">
-                <div className="max-w-prose">
+                <div className="max-w-prose w-full">
                     <h2 className="text-2xl self-start font-bold mb-6">Recent projects</h2>
-                    <p className="max-w-prose text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae sit autem eveniet dolorem quia illum eos. Culpa molestias quis debitis perferendis, tenetur soluta praesentium asperiores, ratione qui rerum facere fugiat velit! Soluta officiis et quas facilis error repellendus corrupti amet ratione laudantium esse quibusdam quis architecto minus harum, porro autem.</p>
-                    <div>
+                    <p className="max-w-prose text-lg">Under construction.</p>
+                    <div className="hidden">
                         <h3>Project title</h3>
                         <p>Project description Lorem ipsum dolor sit amet.</p>
                         <p>technologies used:</p>
@@ -19,6 +19,6 @@ export default function Projects() {
                 </div>
             </section>
             <hr className="border border-zinc-200 w-11/12 max-w-3xl mx-auto" />
-            </>
+        </>
     )
 }
