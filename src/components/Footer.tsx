75b0f2299a28d5ac6ecca91b2221f899df249e97
Amadeja <131427620+amadejaop@users.noreply.github.com>
2024-05-20 10:28:27 +0200

    Add color to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,12 @@
 export default function Footer() {
     return (
-        <footer className="flex flex-col items-center py-10 px-7">
-            <hr className="border border-zinc-200 w-11/12 max-w-3xl mx-auto mb-10" />
+        <footer className="flex flex-col items-center py-10 px-7 bg-pink-600">
             <ul>
-                <li className="list-square pb-2"><a className="link" href="https://www.linkedin.com/in/amadejaop/">Linkedin</a></li>
-                <li className="list-square pb-2"><a className="link" href="https://github.com/amadejaop">GitHub</a></li>
-                <li className="list-square pb-6"><a className="link" href="https://github.com/amadejaop/amadejaop.github.io">Source</a></li>
+                <li className="list-square pb-2 underline"><a className="link" href="https://www.linkedin.com/in/amadejaop/">Linkedin</a></li>
+                <li className="list-square pb-2 underline"><a className="link" href="https://github.com/amadejaop">GitHub</a></li>
+                <li className="list-square pb-6 underline"><a className="link" href="https://github.com/amadejaop/amadejaop.github.io">Source</a></li>
             </ul>
-            <p className="text-sm">Copyright 2024 &copy; <a className="link" href="https://amadejaop.github.io/">Amadeja Obšteter Privošnik</a></p>
+            <p className="text-sm">Copyright 2024 &copy; <a className="link underline" href="https://amadejaop.github.io/">Amadeja Obšteter Privošnik</a></p>
         </footer>
     )
 }
