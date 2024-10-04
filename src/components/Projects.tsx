81c5d1957d450f61a0a1efadaee47631e5da35ff
Amadeja <obsteter.amadeja@gmail.com>
2024-10-04 14:05:51 +0200

    Style first project on the project page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,23 +4,25 @@ export default function Projects() {
             <section id="projects" className="flex flex-col justify-center items-center bg-white py-20 px-7">
                 <div className="max-w-prose w-full">
                     <h2 className="text-2xl self-start font-bold mb-6 text-pink-600">Recent projects</h2>
-                    <div className="w-[150%] mx-[-25%] bg-red-200 flex relative">
-                        <div className="bg-blue-300 absolute w-full top-[25%] p-4"><img src="https://i.ibb.co/QbCVtHF/quendle-small.jpg" alt="Screenshot of the application" className="self-center" width={400} /></div>
-                            <div className="relative w-96 mr-32 ml-auto bg-yellow-100 py-4 px-8">
-                                <h3>Quendle</h3>
-                                <h4>A to do list web application</h4>
+                    <div className="w-[150%] mx-[-20%] flex relative">
+                        <div className="bg-pink-600 absolute w-full top-[25%] p-4 m-0"><a href="https://amadejaop.github.io/vue-todo/#/" className="align-bottom"><img src="https://i.ibb.co/1zRNm4t/quendle-xs.png" alt="Screenshot of the application" className="border border-black hover:opacity-75 transition-all duration-500" width={400} /></a></div>
+                            <div className="relative w-96 mr-32 ml-auto bg-white py-4 px-8 shadow-[5px_5px_0px_0px_rgba(219,39,119)] border border-black">
+                                <h3 className="text-center font-bold text-lg">Quendle</h3>
+                                <h4 className="text-center italic">A to do list web application</h4>
+                                <hr className="m-2" />
                                 <p>A drag and drop to do list inspired by Trello, simplified for everyday tasks and organising your own personal and professional life.</p>
                                 <br />
-                                <p>Features:</p>
+                                <p className="font-bold">Features:</p>
                                 <ul>
                                     <li className="list-disc list-inside">drag and drop</li>
                                     <li className="list-disc list-inside">adding, editing and deleting tasks</li>
                                     <li className="list-disc list-inside">lists saved to local storage</li>
                                     <li className="list-disc list-inside">overview of tasks to be done up to current date</li>
                                     <li className="list-disc list-inside">preview of tasks coming up in the next days</li>
+                                    <li className="list-disc list-inside">dark mode</li>
                                 </ul>
                                 <br/>
-                                <p>Technologies used:</p>
+                                <p className="font-bold">Technologies used:</p>
                                 <ul>
                                     <li className="list-disc list-inside">Vue 3 (composition API) with Vite</li>
                                     <li className="list-disc list-inside">HTML</li>
@@ -28,8 +30,10 @@ export default function Projects() {
                                     <li className="list-disc list-inside">JavaScript</li>
                                 </ul>
                                 <br />
-                                <a href="https://amadejaop.github.io/vue-todo/#/">Live preview</a>
-                                <a href="https://github.com/amadejaop/vue-todo">Source</a>
+                                <div className="flex justify-between">
+                                <a href="https://amadejaop.github.io/vue-todo/#/" className="font-bold hover:text-pink-600 transition-all duration-500">Live preview</a>
+                                <a href="https://github.com/amadejaop/vue-todo" className="font-bold hover:text-pink-600 transition-all duration-500">Source</a>
+                                </div>
                             </div>
                         </div>
                     </div>
